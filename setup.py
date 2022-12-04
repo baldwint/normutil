c8830e2ef5e7566aa9388a4bced2933ae2449bc1
Thomas Baldwin <tkb@sent.com>
2022-12-04 10:43:10 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="normutil",
-    version="0.1.0",
+    version="0.2.0",
     description="Demo package for normconf talk on packaging",
     author="Tom Baldwin",
     author_email="baldwint@baldwint.com",
