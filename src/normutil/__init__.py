e19895f92b9d6193304f246b178d11a67043d19c
Thomas Baldwin <tkb@sent.com>
2022-12-04 11:11:34 -0800

    add code to calculate version at runtime

--- src/normutil/__init__.py
+++ src/normutil/__init__.py
@@ -1,1 +1,9 @@
-from ._version import __version__
+try:
+    from setuptools_scm import get_version
+
+    # We are in a dev environment and can recalculate a fresh version number
+    __version__ = get_version(root="../..", relative_to=__file__)
+    del get_version
+except (ImportError, LookupError):
+    # Otherwise, use the version that was written the last time setup.py ran
+    from ._version import __version__
